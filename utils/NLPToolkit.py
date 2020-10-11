cfdb62145156d6987d15ee753169439f7cc4ad1e
Hans Tananda <hanstananda@gmail.com>
2020-10-11 20:43:28 +0800

    code cleanup, make pipeline better

--- utils/NLPToolkit.py
+++ utils/NLPToolkit.py
@@ -11,49 +11,58 @@ class NLPToolkit:
         'token': [],
         'stemmed_token': [],
         'segmented_sentence': [],
+        'pos_tagger': [],
     }
 
     def __init__(self, document, save_file=""):
-        self.word_tokenizer(document)
-        self.stemmer(self.json_dict['token'])
-        self.sentence_segmentation(document)
-        self.pos_tagger(document, 'pos_tagged_document')
-        # self.pos_tagger(pos_tagging_text_1, 'pos_tagged_sentence_1')
-        # self.pos_tagger(pos_tagging_text_2, 'pos_tagged_sentence_2')
-        # self.pos_tagger(pos_tagging_text_3, 'pos_tagged_sentence_3')
+        word_tokenize_set = self.word_tokenizer(document)
+        for x in word_tokenize_set:
+            self.json_dict['token'].append(x)
+
+        stemmed_token = self.stemmer(self.json_dict['token'])
+        for x in stemmed_token:
+            self.json_dict['stemmed_token'].append(x)
+
+        segmented_sentence = self.sentence_segmentation(document)
+        for x in segmented_sentence:
+            self.json_dict['segmented_sentence'].append(x)
+
+        for i in segmented_sentence:
+            pos_tagged_tokens = self.pos_tagger(i)
+            self.json_dict['pos_tagger'].append(pos_tagged_tokens)
+
         print(self.json_dict)
         if save_file != "":
             self.json_converter(self.json_dict, save_file)
 
     # Tokenizer will have text as an input, inputting the token into a json_file and returning a status.
-    def word_tokenizer(self, document):
+    @staticmethod
+    def word_tokenizer(document):
         word_tokenize_set = set(word_tokenize(document))
-        for x in word_tokenize_set:
-            self.json_dict['token'].append(x)
+        return word_tokenize_set
 
     # Sentence Segmentation will sentence
-    def sentence_segmentation(self, document):
+    @staticmethod
+    def sentence_segmentation(document):
         sentence_tokenizer = PunktSentenceTokenizer()
         sentence_tokenizer.train(document)
         sentence_tokenizer._params.abbrev_types
         segmented_sentence = sentence_tokenizer.tokenize(document)
-        for x in segmented_sentence:
-            self.json_dict['segmented_sentence'].append(x)
+        return segmented_sentence
 
     # Stemmer will stemming the set of token into the set of stemmed_token
-    def stemmer(self, tokens):
+    @staticmethod
+    def stemmer(tokens):
         porter_stemmer = PorterStemmer()
         stemmed_token = [porter_stemmer.stem(token) for token in tokens]
-        for x in stemmed_token:
-            self.json_dict['stemmed_token'].append(x)
+        return stemmed_token
 
     # Pos tagging (Choose 3 sentences by yourself and add it on the global variable
-    def pos_tagger(self, sentence, dict_key):
+    @staticmethod
+    def pos_tagger(sentence):
         tokens = nltk.word_tokenize(sentence)
         pos_tagged_tokens = nltk.pos_tag(tokens)
-        self.json_dict[dict_key] = []
-        for x in pos_tagged_tokens:
-            self.json_dict[dict_key].append(x)
+        return pos_tagged_tokens
 
     # Json_converter will make the dictionary into a json format.
     # Change the file name when you are inputting the different file
