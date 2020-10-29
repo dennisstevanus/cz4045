d50a5fdc4908e9dd4d17e2d08a7112813def947d
Hans Tananda <hanstananda@gmail.com>
2020-10-29 14:07:20 +0800

    changed function-based views to class-based views

--- demo_server/review_generator/views.py
+++ demo_server/review_generator/views.py
@@ -29,23 +29,3 @@ class ReviewGeneratorView(View):
         }
 
         return HttpResponse(self.template.render(context, request))
-
-
-def index(request):
-    template = loader.get_template('review_generator/index.html')
-    context = {
-        'sample': 'abc sample here'
-    }
-    return HttpResponse(template.render(context, request))
-
-
-def process(request):
-    input_text = request.POST['prefix']
-    logging.info(input_text)
-    response = utils.generate_fake_review(input_text)
-    template = loader.get_template('review_generator/index.html')
-    context = {
-        'review': response
-    }
-
-    return HttpResponse(template.render(context, request))
