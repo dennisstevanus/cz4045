0a3437e1d52dfbb59aa5bf7f5dde94513393ac9d
Hans Tananda <hanstananda@gmail.com>
2020-10-29 14:06:55 +0800

    added pages

--- demo_server/review_generator/urls.py
+++ demo_server/review_generator/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path("", views.ReviewGeneratorView.as_view() , name='index'),
+    # path("process/", views.process, name='process'),
 ]
--- demo_server/review_generator/views.py
+++ demo_server/review_generator/views.py
@@ -1,13 +1,51 @@
+import logging
+
 from django.shortcuts import render
 
 # Create your views here.
 
 from django.http import HttpResponse
+from django.template import loader
+from django.views import View
+
+from . import utils
+
+
+class ReviewGeneratorView(View):
+    template = loader.get_template('review_generator/index.html')
+
+    def get(self, request, *args, **kwargs):
+        context = {
+            'sample': 'abc sample here'
+        }
+        return HttpResponse(self.template.render(context, request))
+
+    def post(self, request, *args, **kwargs):
+        input_text = request.POST['prefix']
+        logging.info(input_text)
+        response = utils.generate_fake_review(input_text)
+        context = {
+            'review': response
+        }
+
+        return HttpResponse(self.template.render(context, request))
 
 
 def index(request):
+    template = loader.get_template('review_generator/index.html')
     context = {
-        'sample': ['abc']
+        'sample': 'abc sample here'
     }
-    return render(request, 'review_generator/index.html', context)
+    return HttpResponse(template.render(context, request))
 
+
+def process(request):
+    input_text = request.POST['prefix']
+    logging.info(input_text)
+    response = utils.generate_fake_review(input_text)
+    template = loader.get_template('review_generator/index.html')
+    context = {
+        'review': response
+    }
+
+    return HttpResponse(template.render(context, request))
