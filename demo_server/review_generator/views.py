becf580abbb995c8fd9f0e030b2dfd721315faeb
Hans Tananda <hanstananda@gmail.com>
2020-10-29 14:11:37 +0800

    changed view class to templateview

--- demo_server/review_generator/views.py
+++ demo_server/review_generator/views.py
@@ -7,25 +7,20 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
 from django.views import View
+from django.views.generic import TemplateView
 
 from . import utils
 
 
-class ReviewGeneratorView(View):
-    template = loader.get_template('review_generator/index.html')
-
-    def get(self, request, *args, **kwargs):
-        context = {
-            'sample': 'abc sample here'
-        }
-        return HttpResponse(self.template.render(context, request))
+class ReviewGeneratorView(TemplateView):
+    template_name = 'review_generator/index.html'
 
     def post(self, request, *args, **kwargs):
+        context = self.get_context_data(**kwargs)
         input_text = request.POST['prefix']
         logging.info(input_text)
         response = utils.generate_fake_review(input_text)
-        context = {
-            'review': response
-        }
 
-        return HttpResponse(self.template.render(context, request))
+        context['review'] = response
+
+        return self.render_to_response(context)
