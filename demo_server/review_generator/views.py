a838758bac54fe943a64b2f4d0917abbd900218b
Hans Tananda <hanstananda@gmail.com>
2020-10-29 12:22:38 +0800

    added working html boilerplate

--- demo_server/review_generator/views.py
+++ demo_server/review_generator/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from django.http import HttpResponse
+
+
+def index(request):
+    context = {
+        'sample': ['abc']
+    }
+    return render(request, 'review_generator/index.html', context)
+
