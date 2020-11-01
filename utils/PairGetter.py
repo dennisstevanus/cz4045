65fbac16349d3585760f372af68249d72c84b2f5
Hans Tananda <hanstananda@gmail.com>
2020-11-01 21:36:45 +0800

    remove unused print, pretty print the result

--- utils/PairGetter.py
+++ utils/PairGetter.py
@@ -13,7 +13,7 @@ class PairGetter:
         self.review_list = []
         for line in lines:
             self.review_list.append(line.strip())
-        print(self.review_list)
+        # print(self.review_list)
         # self.review_list = self.review_list[:2]
 
         self.tk = NLPToolkit()
@@ -39,7 +39,7 @@ class PairGetter:
         noun_phrase_list = [(k, {"counter": v["counter"], "review_appearance": v["review_appearance"]}) for k, v in
                             sorted(noun_phrase_map.items(), key=lambda item: item[1]["review_appearance"],
                                    reverse=True)]
-        pprint(noun_phrase_list)
+        # pprint(noun_phrase_list)
         return noun_phrase_list
 
     def get_nouns(self):
@@ -94,7 +94,7 @@ class PairGetter:
                                     result_map[noun]["adj_list"][adj] = 1
 
         result_list = sorted(result_map.items(), key=lambda item: item[1]["adj_count"] * item[1]["counter"], reverse=True)
-        pprint(result_list[:10])
+        # pprint(result_list[:10])
         return result_list, result_map
 
 
@@ -102,4 +102,5 @@ class PairGetter:
 
 if __name__ == "__main__":
     pair_getter = PairGetter()
-    pair_getter.get_nouns_adj_pair()
+    result_list, _ = pair_getter.get_nouns_adj_pair()
+    pprint(result_list[:10])
--- utils/PairRanker.py
+++ utils/PairRanker.py
@@ -31,14 +31,46 @@ class PairRanker:
         else:
             return 0
 
+    def calculate_weighted_result_with_heuristic(self):
+        weighted_result = []
+        mult = 0.1
+        for pair in self.common_pairs:
+            noun = pair[0]
+            noun_count = pair[1]["review_appearance"]
+            cum_pos_adj = 0
+            cum_neg_adj = 0
+            cum_net_adj = 0
+            for adj, adj_count in pair[1]["adj_list"].items():
+                adj_weight = self.get_word_sentiment(adj)
+                if adj_weight < 0:
+                    cum_neg_adj += 1
+                elif adj_weight > 0:
+                    cum_pos_adj += 1
+                else:
+                    cum_net_adj += 1
+            for adj, adj_count in pair[1]["adj_list"].items():
+                adj_weight = self.get_word_sentiment(adj)
+                if adj_weight < 0:
+                    adj_count += cum_neg_adj
+                elif adj_weight > 0:
+                    adj_count += cum_pos_adj
+                else:
+                    adj_count += cum_net_adj
+                negative_multiplier = -1 if adj_weight < 0 else 1
+                count = noun_count * adj_count
+                score = ((abs(adj_weight) * count) * mult + count * (1 - mult)) * negative_multiplier
+                weighted_result.append((score, (noun, adj)))
+        result = sorted(weighted_result, reverse=True)
+        # print(result[:10])
+        # print(result[-10:])
+        return result
+
     def calculate_weighted_result(self):
         weighted_result = []
         mult = 0.1
         for pair in self.common_pairs:
             noun = pair[0]
             noun_count = pair[1]["review_appearance"]
-            for adj, adj_count in pair[1]["adj_list"].items():
-                adj_weight = self.get_word_sentiment(adj)
 
             for adj, adj_count in pair[1]["adj_list"].items():
                 adj_weight = self.get_word_sentiment(adj)
@@ -47,18 +79,27 @@ class PairRanker:
                 score = ((abs(adj_weight) * count) * mult + count * (1 - mult)) * negative_multiplier
                 weighted_result.append((score, (noun, adj)))
         result = sorted(weighted_result, reverse=True)
-        print(result[:5])
-        print(result[-5:])
+        # print(result[:10])
+        # print(result[-10:])
         return result
+
+    @staticmethod
+    def pprint_rank_result(rankings):
+        for rank, item in enumerate(rankings):
+            score, (noun, adj) = item
+            print("{}. < {} - {} > with score of {}".format(rank+1, noun, adj, score))
 
     def get_top_5(self, weighted_results):
         top_5_positive = self.get_distinct_top_5(weighted_results)
         top_5_negative = self.get_distinct_top_5(reversed(weighted_results))
-        print("top 5 positive=", top_5_positive)
-        print("top 5 negative=", top_5_negative)
-        abs_score = sorted([(abs(i[0]), i[1]) for i in top_5_positive+top_5_negative], reverse=True)
+        print("top 5 positive:")
+        self.pprint_rank_result(top_5_positive)
+        print("top 5 negative=")
+        self.pprint_rank_result(top_5_negative)
+        abs_score = sorted([(abs(i[0]), i[1]) for i in top_5_positive + top_5_negative], reverse=True)
         final = self.get_distinct_top_5(abs_score, max_duplicate=2)
-        print(final)
+        print("Final results:")
+        self.pprint_rank_result(final)
         return [i[1] for i in final]
 
     def get_distinct_top_5(self, weighted_results, max_duplicate=1):
@@ -81,10 +122,13 @@ if __name__ == "__main__":
     pair_getter = PairGetter()
     noun_adj_pairs, _ = pair_getter.get_nouns_adj_pair()
     pair_ranker = PairRanker(noun_adj_pairs)
-    weighted_list = pair_ranker.calculate_weighted_result()
-    print("Top 5 pairs: ", pair_ranker.get_top_5(weighted_list))
 
-"""
-[(97.2, ('Netflix', 'new')), (97.2, ('Netflix', 'different')), (79.80000000000001, ('movies', 'popular')), (79.80000000000001, ('movies', 'free')), (68.4, ('Netflix', 'trustworthy'))]
-[(-68.4, ('Netflix', 'fraudulent')), (-68.4, ('Netflix', 'dishonest')), (-68.4, ('Netflix', 'dark')), (-68.4, ('Netflix', 'bad')), (-119.7, ('movies', 'worst'))]
-"""
+    weighted_list = pair_ranker.calculate_weighted_result()
+    print("Top 5 pairs with original scoring system:")
+    result1 = pair_ranker.get_top_5(weighted_list)
+
+    print()
+    weighted_list_with_heuristic = pair_ranker.calculate_weighted_result_with_heuristic()
+    print("Top 5 pairs with heuristic: ")
+    result2 = pair_ranker.get_top_5(weighted_list_with_heuristic)
+
